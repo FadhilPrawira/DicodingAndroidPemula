103124d0951377448092cb35187b79a894faecc6
Fadhil Prawira <fadhilprawira87@gmail.com>
2024-08-10 17:55:28 +0700

    Membuat intent explicit ke halaman lain

--- app/src/main/java/xyz/fadhilprawira/dicodingandroidpemula/MainActivity.kt
+++ app/src/main/java/xyz/fadhilprawira/dicodingandroidpemula/MainActivity.kt
@@ -1,20 +1,26 @@
 package xyz.fadhilprawira.dicodingandroidpemula
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        val btnMoveActivity: Button = findViewById(R.id.btn_move_activity)
+
+        btnMoveActivity.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id){
+            R.id.btn_move_activity -> {
+                val moveIntent = Intent(this@MainActivity, MoveActivity::class.java)
+                startActivity(moveIntent)
+            }
         }
     }
 }
