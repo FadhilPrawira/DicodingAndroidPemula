494daa8179bff9280aa377979167ac4736077a60
Fadhil Prawira <fadhilprawira87@gmail.com>
2024-08-10 17:42:19 +0700

    Membuat calculator BarVolume

--- app/src/main/java/xyz/fadhilprawira/dicodingandroidpemula/MainActivity.kt
+++ app/src/main/java/xyz/fadhilprawira/dicodingandroidpemula/MainActivity.kt
@@ -1,20 +1,74 @@
 package xyz.fadhilprawira.dicodingandroidpemula
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var edtLength: EditText
+    private lateinit var edtWidth: EditText
+    private lateinit var edtHeight: EditText
+    private lateinit var btnCalculate: Button
+    private lateinit var tvResult: TextView
+
+
+    companion object {
+        private const val STATE_RESULT = "state_result"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        edtLength = findViewById(R.id.edt_length)
+        edtWidth = findViewById(R.id.edt_width)
+        edtHeight = findViewById(R.id.edt_height)
+        btnCalculate = findViewById(R.id.btn_calculate)
+        tvResult = findViewById(R.id.tv_result)
+
+        btnCalculate.setOnClickListener(this)
+
+        if(savedInstanceState !=null ){
+            val result = savedInstanceState.getString(STATE_RESULT)
+            tvResult.text = result
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(STATE_RESULT, tvResult.text.toString())
+    }
+    override fun onClick(view: View?) {
+        if (view?.id == R.id.btn_calculate) {
+            val inputLength = edtLength.text.toString().trim()
+            val inputWidth = edtWidth.text.toString().trim()
+            val inputHeight = edtHeight.text.toString().trim()
+            var isEmptyFields = false
+            if (inputLength.isEmpty()) {
+                isEmptyFields = true
+                edtLength.error = getString(R.string.field_ini_tidak_boleh_kosong)
+            }
+
+            if (inputWidth.isEmpty()) {
+                isEmptyFields = true
+                edtWidth.error = getString(R.string.field_ini_tidak_boleh_kosong)
+            }
+
+            if (inputHeight.isEmpty()) {
+                isEmptyFields = true
+                edtHeight.error = getString(R.string.field_ini_tidak_boleh_kosong)
+            }
+
+            if (!isEmptyFields) {
+                val volume = inputLength.toDouble() * inputWidth.toDouble() * inputHeight.toDouble()
+                tvResult.text = volume.toString()
+            }
+
+
         }
     }
 }
