8a4fd07a1649f4e89077bd47992e6606690395b5
Fadhil Prawira <fadhilprawira87@gmail.com>
2024-08-10 22:07:35 +0700

    Membuat intent implisit ke dial phone

--- app/src/main/java/xyz/fadhilprawira/dicodingandroidpemula/MainActivity.kt
+++ app/src/main/java/xyz/fadhilprawira/dicodingandroidpemula/MainActivity.kt
@@ -1,6 +1,7 @@
 package xyz.fadhilprawira.dicodingandroidpemula
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
@@ -18,6 +19,9 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
         val btnMoveWithObject: Button = findViewById(R.id.btn_move_activity_object)
         btnMoveWithObject.setOnClickListener(this)
+
+        val btnDialPhone: Button = findViewById(R.id.btn_dial_number)
+        btnDialPhone.setOnClickListener(this)
     }
 
     override fun onClick(v: View?) {
@@ -45,6 +49,12 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                 val moveWithObjectIntent = Intent(this@MainActivity, MoveWithObjectActivity::class.java)
                 moveWithObjectIntent.putExtra(MoveWithObjectActivity.EXTRA_PERSON, person)
                 startActivity(moveWithObjectIntent)
+            }
+
+            R.id.btn_dial_number -> {
+                val phoneNumber = "08123456789"
+                val dialPhoneIntent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:$phoneNumber"))
+                startActivity(dialPhoneIntent)
             }
         }
     }
